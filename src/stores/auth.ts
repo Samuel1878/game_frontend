3c5c4a73f20e9384e5f201908f06cb23e42b750f
samuel1878e <your-samuelchost006574@gmail.com>
2026-01-28 13:55:56 +0630

    auth

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,12 +1,44 @@
 import { defineStore } from "pinia";
-import { computed, ref } from "vue";
-// export const useAuthStore= defineStore("auth", ()=>{
-//      const name = ref("");
-//      const isLoggedIn = ref(false)
-//    const signIn = computed(({name, password})=>{
-//     name.value = name;
-//     isLoggedIn.value = true
-//    })
+import { ref, computed } from "vue";
 
-//      return { name, isLoggedIn, signIn };
-// });
+export const useAuthStore = defineStore("auth", () => {
+  const user = ref(null);
+  const token = ref(localStorage.getItem("token") || null);
+
+  const isAuthenticated = computed(() => !!token.value);
+
+  function login(userData, accessToken) {
+    user.value = userData;
+    token.value = accessToken;
+
+    localStorage.setItem("token", accessToken);
+    localStorage.setItem("user", JSON.stringify(userData));
+  }
+
+  function loadFromStorage() {
+    const savedUser = localStorage.getItem("user");
+    const savedToken = localStorage.getItem("token");
+
+    if (savedUser && savedToken) {
+      user.value = JSON.parse(savedUser);
+      token.value = savedToken;
+    }
+  }
+
+  function logout() {
+    user.value = null;
+    token.value = null;
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  }
+
+  return {
+    user,
+    token,
+    isAuthenticated,
+    login,
+    logout,
+    loadFromStorage,
+  };
+});
