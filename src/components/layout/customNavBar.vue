da67aac7a8cbdf63c7febf4890779b754a8e0878
Samuel1878 <samuelchost006574@gmail.com>
2026-05-13 05:58:14 +0630

    uat:02:safeAreaView

--- src/components/layout/customNavBar.vue
+++ src/components/layout/customNavBar.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
   title?: string;
   backTo?: string;
 }>();
-const {t} = useI18n();
+const { t } = useI18n();
 const router = useRouter();
 
 const goBack = () => {
@@ -20,30 +20,33 @@ const goBack = () => {
 </script>
 
 <template>
-  <header class="pt-[env(safe-area-inset-top)] flex items-center h-14 px-3 w-full border-b bg-gray-900 
-        bg-linear-to-br from-white/5 via-white/10 to-white/5 sticky z-40 top-0 left-0 right-0
-        backdrop-blur-2xl border-white/5
-        shadow-[0_10px_40px_rgba(0,0,0,0.2)]">
-    <div class="z-10">
-      <button
-        v-if="backTo !== undefined"
-        @click="goBack"
-        class="p-2 flex gap-2 items-center text-white"
+  <nav
+    class="pt-[env(safe-area-inset-top)] w-full border-b bg-gray-900 bg-linear-to-br from-white/5 via-white/10 to-white/5 sticky z-40 top-0 left-0 right-0 backdrop-blur-2xl border-white/5 shadow-[0_10px_40px_rgba(0,0,0,0.2)]"
+  >
+    <div class="flex items-center h-14 px-3 z-40">
+      <div class="z-10">
+        <button
+          v-if="backTo !== undefined"
+          @click="goBack"
+          class="p-2 flex gap-2 items-center text-white"
+        >
+          <chevron-left class="text-white w-6 h-6" />
+          <p>{{ t("back") }}</p>
+        </button>
+      </div>
+
+      <!-- CENTER (Absolute for perfect centering) -->
+      <div
+        v-if="title"
+        class="absolute left-1/2 -translate-x-1/2 text-center font-semibold text-white"
       >
-        <chevron-left class="text-white w-6 h-6" />
-        <p>{{ t('back') }}</p>
-      </button>
-    </div>
+        {{ t(title) }}
+      </div>
 
-    <!-- CENTER (Absolute for perfect centering) -->
-    <div v-if="title" class="absolute left-1/2 -translate-x-1/2 text-center font-semibold text-white">
-      {{ t(title) }}
+      <!-- RIGHT (Actions) -->
+      <div class="ml-auto flex items-center gap-2 z-10 text-white">
+        <slot name="right" />
+      </div>
     </div>
-
-    <!-- RIGHT (Actions) -->
-    <div class="ml-auto flex items-center gap-2 z-10 text-white">
-      <slot name="right" />
-    </div>
-
-  </header>
+  </nav>
 </template>
--- src/gameView.vue
+++ src/gameView.vue
@@ -7,50 +7,49 @@ import { useAuthStore } from "./stores/auth";
 import { hideTawk, showTawk } from "./utils";
 
 const gameStore = useGameStore();
-const authStore = useAuthStore()
+const authStore = useAuthStore();
 const router = useRouter();
 
 // safety: if no URL, go back
 onMounted(() => {
-   hideTawk();
+  hideTawk();
   if (!gameStore.launchUrl) {
     router.replace("/");
   }
 });
-onUnmounted(async() => {
+onUnmounted(async () => {
   // await authStore.init()
   showTawk();
 });
 const goBack = () => {
-    authStore.init()
-    router.back()
-}
+  authStore.init();
+  router.back();
+};
 const goHome = () => {
-    authStore.init()
-    router.replace('/')
-}
+  authStore.init();
+  router.replace("/");
+};
 </script>
 <template>
-  <div class="fixed inset-0 bg-black overflow-hidden">
+  <div class="fixed inset-0 bg-black overflow-hidden flex flex-col">
 
     <!-- TOP BAR -->
     <div
-      class="absolute top-0 left-0 right-0 z-50 h-10
-             flex justify-between items-center px-4
-             bg-gray-900/50 backdrop-blur
-             pt-[env(safe-area-inset-top)]"
+      class="bg-gray-900/50 backdrop-blur pt-[env(safe-area-inset-top)]"
     >
-      <div class="cursor-pointer px-2" @click="goBack">
-        <ChevronLeft class="text-gray-100 w-7 h-7" />
-      </div>
+      <div class="h-10 flex justify-between items-center px-4">
+        <div class="cursor-pointer px-2" @click="goBack">
+          <ChevronLeft class="text-gray-100 w-7 h-7" />
+        </div>
 
-      <div class="cursor-pointer flex items-center" @click="goHome">
-        <img src="/logo.png" class="h-8" />
+        <div class="cursor-pointer flex items-center" @click="goHome">
+          <img src="/logo.png" class="h-8" />
+        </div>
       </div>
     </div>
 
     <!-- GAME -->
-    <div class="absolute inset-0 pt-[calc(2.25rem+env(safe-area-inset-top))]">
+    <div class="flex-1 min-h-0">
       <iframe
         v-if="gameStore.launchUrl"
         :src="gameStore.launchUrl"
